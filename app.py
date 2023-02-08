1d80b4d0d40da1939339c5f222d20524876ac72a
katemurraay <117349221@umail.ucc.ie>
2023-02-08 14:45:55 +0000

    first commit

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
     return render_template('index.html')
+
+@app.route('/h')
+def hello():
+    return 'Hello World'
