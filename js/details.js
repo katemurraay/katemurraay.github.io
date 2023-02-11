f5a6400997098b943c2502a4c5bd9336ebb10e24
katemurraay <117349221@umail.ucc.ie>
2023-02-11 16:34:21 +0000

    Header Update

--- js/details.js
+++ js/details.js
@@ -2,12 +2,14 @@ window.onload = function(){
     var email = "mailto: katem.murray1998@gmail.com";
     var linkedin = "https://www.linkedin.com/in/kate-murray-343138151/";
     var github = "https://github.com/katemurraay";
-    
+   
 
-    document.getElementByClassName('email').href = email;
-    document.getElementByClassName('linkedin').href = linkedin;
-    document.getElementByClassName('github').href = github;
-
+    document.getElementById('header_email').href = email;
+    document.getElementById('header_linkedin').href = linkedin;
+    document.getElementById('header_github').href = github;
+    document.getElementById('footer_email').href = email;
+    document.getElementById('footer_linkedin').href = linkedin;
+    document.getElementById('footer_github').href = github;
 };
 
 
